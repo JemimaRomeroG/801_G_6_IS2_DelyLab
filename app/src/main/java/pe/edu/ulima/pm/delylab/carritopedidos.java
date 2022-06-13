1807089afce1e256d181d5a0edd344c28836b5c2
JemimaRomeroG <89671253+JemimaRomeroG@users.noreply.github.com>
2022-06-13 18:33:48 -0500

    Commit Jemi

---
+++ app/src/main/java/pe/edu/ulima/pm/delylab/carritopedidos.java
@@ -0,0 +1,13 @@
+package pe.edu.ulima.pm.delylab;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class carritopedidos extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.carritopedidos);
+    }
+}
